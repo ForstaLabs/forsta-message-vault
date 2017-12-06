1c0d5902e33d896d1b3ad94a51e83a8a0e03595f
Justin Mayfield <tooker@gmail.com>
2017-12-05 21:12:16 -0700

    Use our builds of semantic and jquery

--- server/web/index.js
+++ server/web/index.js
@@ -1,29 +1,34 @@
-const api = require('api');
+const api = require('./api');
 const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const process = require('process');
 
-const root = `${__dirname}/../..`;
+const root = `${__dirname}/../../dist`;
+const build = require(`${root}/build.json`);
 const port = process.env.PORT || '4096';
 
 
 class WebServer {
 
     constructor() {
+        const jsenv = {};
+        for (const key of Object.keys(build)) {
+            jsenv[key.toUpperCase()] = build[key];
+        }
         this.port = port;
         this.app = express();
         this.app.use(morgan('dev')); // logging
         this.app.use(bodyParser.json());
         this.app.use('/v1/', (new api.V1Handler(this)).router);
         this.app.use('/static/', express.static(path.join(root, 'static'), {strict: true}));
-        this.app.get('/*', (req, res) => res.sendFile('static/main.html', {root}));
-        this.app.get('/install', (req, res) => res.sendFile('static/install.html', {root}));
         this.app.get('/env.js', (req, res) => {
             res.setHeader('Content-Type', 'application/javascript');
-            res.send(`self.F = self.F || {}; F.env = ${JSON.stringify({})};\n`);
+            res.send(`self.F = self.F || {}; F.env = ${JSON.stringify(jsenv)};\n`);
         });
+        this.app.get('/install', (req, res) => res.sendFile('static/html/install.html', {root}));
+        this.app.get('/*', (req, res) => res.sendFile('static/html/main.html', {root}));
         this.app.use((req, res, next) => {
             res.status(404).json({
                 error: 'bad_request',
