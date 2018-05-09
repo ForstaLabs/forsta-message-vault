0190cf5bc6dcef5b2f50478dd331c0b0ba699c90
Justin Mayfield <tooker@gmail.com>
2018-05-09 16:48:02 -0600

    Slogging through windows compat.

--- electron/main.js
+++ electron/main.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const os = require('os');
 const pkg = require('../package');
 const childProcess = require('child_process');
-const {app, BrowserWindow, Tray, nativeImage, shell, Menu} = require('electron');
+const {app, BrowserWindow, Tray, nativeImage, shell, Menu, dialog} = require('electron');
 const path = require('path');
 const process = require('process');
 const platform = os.platform();
@@ -13,15 +13,23 @@ const menu = require('./menu');
 const pgdata = path.join(os.homedir(), '.forsta_message_vault_pgdata');
 const pgsql = path.join(__dirname, 'pgsql');
 const pgconf = path.join(__dirname, 'pgconf');
-const pgsock = fs.mkdtempSync(path.join(os.tmpdir(), '/vaultdb-'));
+const pgsock = fs.mkdtempSync(path.join(os.tmpdir(), 'vaultdb-'));
 const pgConfData = [
     `listen_addresses = ''`,
     `unix_socket_directories = '${pgsock}'`
 ].join('\n');
 
 const port = Number(process.env['PORT']) || 14096;
-const imagesDir = path.join(__dirname, '../dist/static/images/');
-const appIcon = nativeImage.createFromPath(imagesDir + 'logo.png');
+const imagesDir = path.join(__dirname, '..', 'dist', 'static', 'images');
+const appIcon = nativeImage.createFromPath(path.join(imagesDir, 'logo.png'));
+
+let win;
+let started;
+
+process.on('unhandledRejection', ev => {
+    console.error(ev.reason);
+    dialog.showErrorBox('Unhandled Rejection', ev.reason);
+});
 
 async function sleep(seconds) {
     await new Promise(resolve => setTimeout(resolve, seconds * 1000));
@@ -31,12 +39,12 @@ async function initDatabase() {
     let needCreate;
     if (!fs.existsSync(pgdata)) {
         console.log("Setup database...", __dirname);
-        console.log(childProcess.execFileSync(`${pgsql}/bin/initdb`, [pgdata], {encoding: 'utf8'}));
-        fs.copyFileSync(`${pgconf}/pg_hba.conf`, `${pgdata}/pg_hba.conf`);
+        console.log(childProcess.execFileSync(path.join(pgsql, 'bin', 'initdb'), [pgdata], {encoding: 'utf8'}));
+        fs.copyFileSync(path.join(pgconf, 'pg_hba.conf'), path.join(pgdata, 'pg_hba.conf'));
         needCreate = true;
     }
-    fs.writeFileSync(`${pgdata}/postgresql.conf`, pgConfData);
-    const dbProc = childProcess.execFile(`${pgsql}/bin/postgres`, ['-D', pgdata]);
+    fs.writeFileSync(path.join(pgdata, 'postgresql.conf'), pgConfData);
+    const dbProc = childProcess.execFile(path.join(pgsql, 'bin', 'postgres'), ['-D', pgdata]);
     console.log('Started PostgreSQL PID:', dbProc.pid);
     dbProc.stdout.on('data', x => console.info("DB: " + x));
     dbProc.stderr.on('data', x => console.warn("DB [E]: " + x));
@@ -46,11 +54,10 @@ async function initDatabase() {
     await sleep(1); // XXX timing hack to wait for db ready state.
     if (needCreate) {
         console.warn("Creating NEW database");
-        console.log(childProcess.execFileSync(`${pgsql}/bin/createdb`, ['-h', pgsock], {encoding: 'utf8'}));
+        console.log(childProcess.execFileSync(path.join(pgsql, 'bin', 'createdb'), ['-h', pgsock], {encoding: 'utf8'}));
     }
 }
 
-let win;
 
 if (app.dock) {
     app.dock.setIcon(appIcon);
@@ -65,6 +72,10 @@ function showWindow() {
 }
 
 function createWindow() {
+    if (!started) {
+        console.warn("Ignore early createWindow attempt");
+        return;
+    }
     console.info("Creating Window");
     win = new BrowserWindow({ 
         width: 1024,
@@ -103,18 +114,32 @@ app.once('ready', async () => {
     // This method will be called when Electron has finished
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
-    await initDatabase();
+    try {
+        await initDatabase();
+    } catch(e) {
+        console.error("Database startup error: " + e.message);
+        dialog.showErrorBox("Database startup error", e.message);
+        process.exit(1);
+        return;
+    }
     process.env.PORT = port;
     process.env.RELAY_STORAGE_BACKING = 'postgres';
     process.env.DATABASE_URL = pgsock;
-    await require('../server');
+    try {
+        await require('../server');
+    } catch(e) {
+        console.error("Web server startup error: " + e.message);
+        dialog.showErrorBox("Web server startup error", e.message);
+        process.exit(1);
+        return;
+    }
 
     Menu.setApplicationMenu(menu)
 
     const tray = new Tray(appIcon);
     tray.setToolTip(pkg.description || 'Forsta Message Vault');
     tray.on('click', showWindow);
-
+    started = true;
     createWindow();
 });
 app.on('before-quit', () => {
